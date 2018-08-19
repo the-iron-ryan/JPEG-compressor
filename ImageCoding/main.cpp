31a8f8bed66bc661681d5ac9b524dc1ced47157d
Ryan Dougherty <ryan.dougherty@utah.edu>
2018-08-19 10:01:30 -0600

    Began work on multicolored compression

--- ImageCoding/main.cpp
+++ ImageCoding/main.cpp
@@ -12,7 +12,7 @@ unsigned int g_windowWidth = 600;
 unsigned int g_windowHeight = 600;
 char* g_windowName = "HW2-Transform-Coding-Image";
 
-#define IMAGE_FILE "data/cameraman.ppm"
+#define IMAGE_FILE "data/lenna.ppm"
 
 GLFWwindow* g_window;
 
@@ -25,10 +25,26 @@ std::vector<float> g_luminance_data;
 std::vector<float> g_compressed_luminance_data;
 std::vector<float> g_haar_compressed_luminance_data;
 
-struct color
+std::vector<float> g_Y_data;
+std::vector<float> g_Cb_data;
+std::vector<float> g_Cr_data;
+
+std::vector<float> g_compressed_Y_data;
+std::vector<float> g_compressed_Cb_data;
+std::vector<float> g_compressed_Cr_data;
+
+
+
+struct color_RGB
 {
 	unsigned char r, g, b;
 };
+struct color_YCbCr
+{
+	unsigned char y, cb, cr;
+};
+
+std::vector<color_RGB> finalImgRGBData;
 
 int g_draw_origin = 1;
 
@@ -49,11 +65,22 @@ void printMatrix(const float* a, int width, int height)
 		for (int w = 0; w < width; w++)
 		{
 			cout << setw(8) << setfill(' ') << setprecision(4)
-				<< a[h * width + w]; 
+				<< a[h * width + w];
 			if (w < width - 1)
 				cout << ", ";
 		}
 		cout << "]" << endl;
+	}
+
+}
+void shiftMatrixByValue(float* &a, int width, int height, float shiftValue)
+{
+	for (int h = 0; h < height; h++)
+	{
+		for (int w = 0; w < width; w++)
+		{
+			a[h * width + w] += shiftValue;
+		}
 	}
 
 }
@@ -125,14 +152,14 @@ float* multiply(const float* a, float* b, int width, int height)
 	return mat;
 }
 
-void DCT(const float* imgBlockMat, float* dctMat, int u, int v, int m, int blockSize)
+void DCT(const float* imgBlockMat, float* dctMat, int u, int v, int m)
 {
 	// Zero out value
 	dctMat[v * 8 + u] = 0;
 
-	for (int y = 0; y < blockSize; y++)
+	for (int y = 0; y < 8; y++)
 	{
-		for (int x = 0; x < blockSize; x++)
+		for (int x = 0; x < 8; x++)
 		{
 			float c_u = 1.0f, c_v = 1.0f;
 
@@ -142,27 +169,15 @@ void DCT(const float* imgBlockMat, float* dctMat, int u, int v, int m, int block
 			if (v == 0)
 				c_v = 1.0f / sqrt(2.0f);
 
-			cout << endl;
-			cout << "img: " << endl;
-			printMatrix(imgBlockMat, 8, 8);
-			cout << endl;
-			cout << "before: " << endl;
-			printMatrix(dctMat, 8, 8);
-
-
-			//if (u + v <= m || m == 0)
-			dctMat[v * 8 + u] += 0.25f * c_u * c_v * imgBlockMat[y * blockSize + x] * cos((u * M_PI) * (2.0f * x + 1.0f) / 16.0f) * cos((v * M_PI) * (2.0f * y + 1.0f) / 16.0f);
-			cout << "Dct val " << dctMat[v * 8 + u] << endl;
-			cout << endl;
-			cout << "after: " << endl;
-			printMatrix(dctMat, 8, 8);
+			if (u + v <= m || m == 0)
+				dctMat[v * 8 + u] += 0.25f * c_u * c_v * imgBlockMat[y * 8 + x] * cos((u * M_PI) * (2.0f * x + 1.0f) / 16.0f) * cos((v * M_PI) * (2.0f * y + 1.0f) / 16.0f);
 		}
 	}
 
 }
-void IDCT(float* imgBlockMat, const float* dctMat, int x, int y, int blockSize)
+void IDCT(float* imgBlockMat, const float* dctMat, int x, int y)
 {
-	imgBlockMat[y * blockSize + x] = 0;
+	imgBlockMat[y * 8 + x] = 0;
 
 	for (int v = 0; v < 8; v++)
 	{
@@ -176,7 +191,7 @@ void IDCT(float* imgBlockMat, const float* dctMat, int x, int y, int blockSize)
 			if (v == 0)
 				c_v = 1.0f / sqrt(2.0f);
 
-			imgBlockMat[y * blockSize + x] += 0.25f * c_u * c_v * dctMat[v * 8 + u] * cos((u * M_PI) * (2.0f * x + 1) / 16.0f) * cos((v * M_PI) * (2.0f * y + 1.0f) / 16.0f);
+			imgBlockMat[y * 8 + x] += 0.25f * c_u * c_v * dctMat[v * 8 + u] * cos((u * M_PI) * (2.0f * x + 1) / 16.0f) * cos((v * M_PI) * (2.0f * y + 1.0f) / 16.0f);
 		}
 
 	}
@@ -200,14 +215,14 @@ void CompressBlockHaar(const float* A, float* &B, float compressionRatio)
 	//cout << "H: " << endl;
 	// Create standard Haar matrix
 	float *H = new float[8 * 8]
-	{	0.125f,  0.125f,  0.25f,   0.0f,  0.5f,   0.0f,   0.0f,  0.0f,
+	{ 0.125f,  0.125f,  0.25f,   0.0f,  0.5f,   0.0f,   0.0f,  0.0f,
 		0.125f,  0.125f,  0.25f,   0.0f, -0.5f,   0.0f,   0.0f,  0.0f,
 		0.125f,  0.125f, -0.25f,   0.0f,  0.0f,   0.5f,   0.0f,	 0.0f,
 		0.125f,  0.125f, -0.25f,   0.0f,  0.0f,  -0.5f,   0.0f,  0.0f,
 		0.125f, -0.125f,   0.0f,  0.25f,  0.0f,   0.0f,   0.5f,  0.0f,
 		0.125f, -0.125f,   0.0f,  0.25f,  0.0f,   0.0f,  -0.5f,  0.0f,
 		0.125f, -0.125f,   0.0f, -0.25f,  0.0f,   0.0f,   0.0f,  0.5f,
-		0.125f, -0.125f,   0.0f, -0.25f,  0.0f,   0.0f,   0.0f, -0.5f};
+		0.125f, -0.125f,   0.0f, -0.25f,  0.0f,   0.0f,   0.0f, -0.5f };
 	normalizeMatrix(H, 8, 8);
 
 	//cout << "H_t: " << endl;
@@ -228,7 +243,7 @@ void CompressBlockHaar(const float* A, float* &B, float compressionRatio)
 
 	float* AHH_t = multiply(H_t, multiply(A, H, 8, 8), 8, 8);
 
-	
+
 	//cout << "Matrix after haar2: " << endl;
 	//printMatrix(B, 8, 8);
 
@@ -239,12 +254,12 @@ void CompressBlockHaar(const float* A, float* &B, float compressionRatio)
 	B = multiply(H, multiply(AHH_t, H_t, 8, 8), 8, 8);
 }
 
-void CompressBlock(const float* A, float* B, int m, int blockSize)
+void CompressBlock(const float* A, float* B, int m)
 {
-	float *C = new float[blockSize * blockSize];
+	float *C = new float[8 * 8];
 
 	//cout << "Mat before DCT: " << endl;
-	//printMatrix(A, blockSize, blockSize);
+	//printMatrix(A, 8, 8);
 	//cout << endl;
 
 	// DCT
@@ -252,29 +267,24 @@ void CompressBlock(const float* A, float* B, int m, int blockSize)
 	{
 		for (int u = 0; u < 8; u++)
 		{
-			DCT(A, C, u, v, m, blockSize);
+			DCT(A, C, u, v, m);
 		}
 	}
-	cout << "Mat after DCT: " << endl;
-	printMatrix(C, blockSize, blockSize);
-	cout << endl;
+	//cout << "Mat after DCT: " << endl;
+	//printMatrix(C, 8, 8);
+	//cout << endl;
 
 	// IDCT
-	for (int y = 0; y < blockSize; y++)
+	for (int y = 0; y < 8; y++)
 	{
-		for (int x = 0; x < blockSize; x++)
+		for (int x = 0; x < 8; x++)
 		{
-			IDCT(B, C, x, y, blockSize);
+			IDCT(B, C, x, y);
 		}
 	}
 	//cout << "Mat after IDCT: " << endl;
-	//printMatrix(B, blockSize, blockSize);
+	//printMatrix(B, 8, 8);
 	//cout << endl;
-
-	// m represents a particular frequency that we must zero out, we zero out if 
-	//    we get an x, y index of the matrix when added together that is > m
-
-	// TODO: Homework Task 2 (see the PDF description)
 
 }
 
@@ -286,7 +296,7 @@ void GrabBlock(const std::vector<float> &img, float *block, int x, int y, int wi
 	{
 		for (int w = x; w < x + width; w++)
 		{
-			block[i++] = img[h * g_image_width + w];
+			block[i++] = img[h * g_image_width + w] - 0.5f;
 		}
 	}
 }
@@ -298,7 +308,7 @@ void PlaceBlock(std::vector<float> &img, const float *block, int x, int y, int w
 	{
 		for (int w = x; w < x + width; w++)
 		{
-			img[h * g_image_width + w] = block[i++];
+			img[h * g_image_width + w] = block[i++] + 0.5f;
 		}
 	}
 }
@@ -315,19 +325,22 @@ void PrintImage(const vector<float> img)
 		cout << "]" << endl << endl;
 	}
 }
-void CompressImage(const std::vector<float> I, std::vector<float>& O, int m, int blockSize)
+void CompressImage(const std::vector<float> I, std::vector<float>& O, int m)
 {
-	for (int imgY = 0; imgY < g_image_height; imgY += blockSize)
+	for (int imgY = 0; imgY < g_image_height; imgY += 8)
 	{
-		for (int imgX = 0; imgX < g_image_width; imgX += blockSize)
+		for (int imgX = 0; imgX < g_image_width; imgX += 8)
 		{
-			float *block = new float[blockSize * blockSize];
-			float *compressedBlock = new float[blockSize * blockSize];
-			GrabBlock(I, block, imgX, imgY, blockSize, blockSize);
+			float *block = new float[8 * 8];
+			float *compressedBlock = new float[8 * 8];
 
-			CompressBlock(block, compressedBlock, m, blockSize);
+			GrabBlock(I, block, imgX, imgY, 8, 8);
+			shiftMatrixByValue(block, 8, 8, -128);
 
-			PlaceBlock(O, compressedBlock, imgX, imgY, blockSize, blockSize);
+			CompressBlock(block, compressedBlock, m);
+
+			shiftMatrixByValue(compressedBlock, 8, 8, +128);
+			PlaceBlock(O, compressedBlock, imgX, imgY, 8, 8);
 		}
 	}
 }
@@ -342,6 +355,7 @@ void CompressImageHaar(const std::vector<float> I, std::vector<float>& O, float 
 			GrabBlock(I, block, imgX, imgY, 8, 8);
 
 			CompressBlockHaar(block, compressedBlock, r);
+
 			PlaceBlock(O, compressedBlock, imgX, imgY, 8, 8);
 		}
 	}
@@ -362,7 +376,7 @@ int ReadLine(FILE *fp, int size, char *buffer)
 
 //-------------------------------------------------------------------------------
 
-bool LoadPPM(FILE *fp, int &width, int &height, std::vector<color> &data)
+bool LoadPPM(FILE *fp, int &width, int &height, std::vector<color_RGB> &data)
 {
 	const int bufferSize = 1024;
 	char buffer[bufferSize];
@@ -378,7 +392,7 @@ bool LoadPPM(FILE *fp, int &width, int &height, std::vector<color> &data)
 	while (buffer[0] == '#') ReadLine(fp, bufferSize, buffer);  // skip comments
 
 	data.resize(width*height);
-	fread(data.data(), sizeof(color), width*height, fp);
+	fread(data.data(), sizeof(color_RGB), width*height, fp);
 
 	return true;
 }
@@ -451,10 +465,10 @@ void render()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	if (g_draw_origin == 1)
-		glDrawPixels(g_image_width, g_image_height, GL_LUMINANCE, GL_FLOAT, &g_luminance_data[0]);
-	else if(g_draw_origin == 2)
+		glDrawPixels(g_image_width, g_image_height, GL_RGB, GL_3_BYTES, &finalImgRGBData[0]);
+	else if (g_draw_origin == 2)
 		glDrawPixels(g_image_width, g_image_height, GL_LUMINANCE, GL_FLOAT, &g_compressed_luminance_data[0]);
-	else if(g_draw_origin == 3)
+	else if (g_draw_origin == 3)
 		glDrawPixels(g_image_width, g_image_height, GL_LUMINANCE, GL_FLOAT, &g_haar_compressed_luminance_data[0]);
 }
 
@@ -477,7 +491,7 @@ void renderLoop()
 
 bool loadImage()
 {
-	std::vector<color> g_image_data;
+	std::vector<color_RGB> g_image_data;
 	g_image_data.clear();
 	g_image_width = 0;
 	g_image_height = 0;
@@ -488,6 +502,15 @@ bool loadImage()
 	success = LoadPPM(fp, g_image_width, g_image_height, g_image_data);
 
 	g_luminance_data.resize(g_image_width * g_image_height);
+
+	g_Y_data.resize(g_image_width * g_image_height);
+	g_Cb_data.resize(g_image_width * g_image_height);
+	g_Cr_data.resize(g_image_width * g_image_height);
+
+	g_compressed_Y_data.resize(g_image_width * g_image_height);
+	g_compressed_Cb_data.resize(g_image_width * g_image_height);
+	g_compressed_Cr_data.resize(g_image_width * g_image_height);
+
 	g_compressed_luminance_data.resize(g_image_width * g_image_height);
 	g_haar_compressed_luminance_data.resize(g_image_width * g_image_height);
 	for (int i = 0; i < g_image_height; i++)
@@ -495,7 +518,19 @@ bool loadImage()
 		for (int j = 0; j < g_image_width; j++)
 		{
 			// the index are not matching because of the difference between image space and OpenGl screen space
-			g_luminance_data[i* g_image_width + j] = g_image_data[(g_image_height - i - 1)* g_image_width + j].r / 255.0f;
+			//g_luminance_data[i* g_image_width + j] = g_image_data[(g_image_height - i - 1)* g_image_width + j].r / 255.0f;
+			int curIndex = (g_image_height - i - 1)* g_image_width + j;
+			float red = g_image_data[curIndex].r;
+			float blue = g_image_data[curIndex].b;
+			float green = g_image_data[curIndex].g;
+
+			//TODO: take away 255 scaling
+			//g_luminance_data[i * g_image_width + j] = (0.299f * red + 0.587f * green + 0.114f * blue) / 255.0f;
+
+			// Convert to CBCR
+			g_Y_data[i * g_image_width + j] = 0.299f * red + 0.587f * green + 0.114f * blue;
+			g_Cb_data[i * g_image_width + j] = -0.16787f * red - 0.331264f * green + 0.5f * blue + 128;
+			g_Cr_data[i * g_image_width + j] = 0.5f * red - 0.418688 * green - 0.081312 * blue + 128;
 		}
 	}
 
@@ -507,21 +542,37 @@ bool loadImage()
 
 bool writeImage()
 {
-	std::vector<color> tmpData;
-	tmpData.resize(g_image_width * g_image_height);
+	finalImgRGBData.resize(g_image_width * g_image_height);
 
 	for (int i = 0; i < g_image_height; i++)
 	{
 		for (int j = 0; j < g_image_width; j++)
 		{
 			// make sure the value will not be larger than 1 or smaller than 0, which might cause problem when converting to unsigned char
-			float tmp = g_compressed_luminance_data[i* g_image_width + j];
-			if (tmp < 0.0f)	tmp = 0.0f;
-			if (tmp > 1.0f)	tmp = 1.0f;
+			//float tmp = g_compressed_luminance_data[i* g_image_width + j];
+			//if (tmp < 0.0f)	tmp = 0.0f;
+			//if (tmp > 1.0f)	tmp = 1.0f;
 
-			tmpData[(g_image_height - i - 1)* g_image_width + j].r = unsigned char(tmp * 255.0);
-			tmpData[(g_image_height - i - 1)* g_image_width + j].g = unsigned char(tmp * 255.0);
-			tmpData[(g_image_height - i - 1)* g_image_width + j].b = unsigned char(tmp * 255.0);
+			//tmpData[(g_image_height - i - 1)* g_image_width + j].r = unsigned char(tmp * 255.0);
+			//tmpData[(g_image_height - i - 1)* g_image_width + j].g = unsigned char(tmp * 255.0);
+			//tmpData[(g_image_height - i - 1)* g_image_width + j].b = unsigned char(tmp * 255.0);
+
+			float Y = g_compressed_Y_data[i* g_image_width + j];
+			float Cb = g_compressed_Cb_data[i* g_image_width + j];
+			float Cr = g_compressed_Cr_data[i* g_image_width + j];
+
+			float red = Y + 1.402f * (Cr - 128);
+			float green = Y - 0.344136f * (Cb - 128) - 0.714136f * (Cr - 128);
+			float blue = Y + 1.772f * (Cb - 128);
+
+			finalImgRGBData[(g_image_height - i - 1)* g_image_width + j].r = unsigned char(red);
+			finalImgRGBData[(g_image_height - i - 1)* g_image_width + j].g = unsigned char(green);
+			finalImgRGBData[(g_image_height - i - 1)* g_image_width + j].b = unsigned char(blue);
+
+			//tmpData[(g_image_height - i - 1)* g_image_width + j].r = unsigned char(valY);
+			//tmpData[(g_image_height - i - 1)* g_image_width + j].g = unsigned char(valCb);
+			//tmpData[(g_image_height - i - 1)* g_image_width + j].b = unsigned char(tmp * 255.0);
+
 		}
 	}
 
@@ -531,14 +582,14 @@ bool writeImage()
 	fprintf(fp, "P6\r");
 	fprintf(fp, "%d %d\r", g_image_width, g_image_height);
 	fprintf(fp, "255\r");
-	fwrite(tmpData.data(), sizeof(color), g_image_width * g_image_height, fp);
+	fwrite(finalImgRGBData.data(), sizeof(color_RGB), g_image_width * g_image_height, fp);
 
 	return true;
 }
 bool writeImageHaar()
 {
-	std::vector<color> tmpData;
-	tmpData.resize(g_image_width * g_image_height);
+	std::vector<color_RGB> finalImgRGBData;
+	finalImgRGBData.resize(g_image_width * g_image_height);
 
 	for (int i = 0; i < g_image_height; i++)
 	{
@@ -549,9 +600,9 @@ bool writeImageHaar()
 			if (tmp < 0.0f)	tmp = 0.0f;
 			if (tmp > 1.0f)	tmp = 1.0f;
 
-			tmpData[(g_image_height - i - 1)* g_image_width + j].r = unsigned char(tmp * 255.0);
-			tmpData[(g_image_height - i - 1)* g_image_width + j].g = unsigned char(tmp * 255.0);
-			tmpData[(g_image_height - i - 1)* g_image_width + j].b = unsigned char(tmp * 255.0);
+			finalImgRGBData[(g_image_height - i - 1)* g_image_width + j].r = unsigned char(tmp * 255.0);
+			finalImgRGBData[(g_image_height - i - 1)* g_image_width + j].g = unsigned char(tmp * 255.0);
+			finalImgRGBData[(g_image_height - i - 1)* g_image_width + j].b = unsigned char(tmp * 255.0);
 		}
 	}
 
@@ -561,7 +612,7 @@ bool writeImageHaar()
 	fprintf(fp, "P6\r");
 	fprintf(fp, "%d %d\r", g_image_width, g_image_height);
 	fprintf(fp, "255\r");
-	fwrite(tmpData.data(), sizeof(color), g_image_width * g_image_height, fp);
+	fwrite(finalImgRGBData.data(), sizeof(color_RGB), g_image_width * g_image_height, fp);
 
 	return true;
 }
@@ -572,33 +623,33 @@ bool writeImageHaar()
 //-----------------------------------------------------------
 void TestCompressBlock()
 {
-	float *A = new float[8 * 8] 
-		{-76.0f, -73.0f, -67.0f, -62.0f, -58.0f, -67.0f, -64.0f, -55.0f,
-		 -65.0f, -69.0f, -73.0f, -38.0f, -19.0f, -43.0f, -59.0f, -56.0f,
-		 -66.0f, -69.0f, -60.0f, -15.0f,  16.0f, -24.0f, -62.0f, -55.0f,
-		 -65.0f, -70.0f, -57.0f,  -6.0f,  26.0f, -22.0f, -58.0f, -59.0f,
-		 -61.0f, -67.0f, -60.0f, -24.0f,  -2.0f, -40.0f, -60.0f, -58.0f,
-		 -49.0f, -63.0f, -68.0f, -58.0f, -51.0f, -60.0f, -70.0f, -53.0f,
-		 -43.0f, -57.0f, -64.0f, -69.0f, -73.0f, -67.0f, -63.0f, -45.0f,
-		 -41.0f, -49.0f, -59.0f, -60.0f, -63.0f, -52.0f, -50.0f, -34.0f};
+	float *A = new float[8 * 8]
+	{ -76.0f, -73.0f, -67.0f, -62.0f, -58.0f, -67.0f, -64.0f, -55.0f,
+		-65.0f, -69.0f, -73.0f, -38.0f, -19.0f, -43.0f, -59.0f, -56.0f,
+		-66.0f, -69.0f, -60.0f, -15.0f,  16.0f, -24.0f, -62.0f, -55.0f,
+		-65.0f, -70.0f, -57.0f,  -6.0f,  26.0f, -22.0f, -58.0f, -59.0f,
+		-61.0f, -67.0f, -60.0f, -24.0f,  -2.0f, -40.0f, -60.0f, -58.0f,
+		-49.0f, -63.0f, -68.0f, -58.0f, -51.0f, -60.0f, -70.0f, -53.0f,
+		-43.0f, -57.0f, -64.0f, -69.0f, -73.0f, -67.0f, -63.0f, -45.0f,
+		-41.0f, -49.0f, -59.0f, -60.0f, -63.0f, -52.0f, -50.0f, -34.0f };
 
 	float *B = new float[8 * 8];
 
 	printMatrix(A, 8, 8);
 	cout << endl;
-	CompressBlock(A, B, 8, 8);
+	CompressBlock(A, B, 8);
 }
 void TestCompressBlockHaar()
 {
 	float *A = new float[8 * 8]
-	{	88,88,89,90,92,94,96,97,
+	{ 88,88,89,90,92,94,96,97,
 		90,90,91,92,93,95,97,97,
 		92,92,93,94,95,96,97,97,
 		93,93,94,95,96,96,96,96,
 		92,93,95,96,96,96,96,95,
 		92,94,96,98,99,99,98,97,
 		94,96,99,101,103,103,102,101,
-		95,97,101,104,106,106,105,105};
+		95,97,101,104,106,106,105,105 };
 
 
 	float *B = new float[8 * 8];
@@ -612,12 +663,16 @@ int main()
 	loadImage();
 
 
-	int n = 3;	// TODO: change the parameter n from 1 to 16 to see different image quality
-	CompressImage(g_luminance_data, g_compressed_luminance_data, n, 8);	
-	CompressImageHaar(g_luminance_data, g_haar_compressed_luminance_data, 0.25f);
+	int n = 2;	// TODO: change the parameter n from 1 to 16 to see different image quality
+				//CompressImage(g_luminance_data, g_compressed_luminance_data, n);	
+				//CompressImageHaar(g_luminance_data, g_haar_compressed_luminance_data, 0.25f);
+
+	CompressImage(g_Y_data, g_compressed_Y_data, n);
+	CompressImage(g_Cb_data, g_compressed_Cb_data, n);
+	CompressImage(g_Cr_data, g_compressed_Cr_data, n);
 
 	writeImage();
-	writeImageHaar();
+	//writeImageHaar();
 
 	// render loop
 	initWindow();
